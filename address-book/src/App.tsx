0c3e009f4bc463c638c35b78bce4886e3ee00330
Andrew W. Benson <awbenson@gmail.com>
2022-10-04 21:13:19 -0400

    Init Commit - Start of Design Phrase

---
+++ address-book/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Address Book example coming soon. Doing the design work first, so check the reference section.  Do note that you may need to have Bootstrap Studio for the base file until it's designed.  Then I will export the design into HTML/CSS to serve as the design mocks for the site.</p>
+    </div>
+  );
+}
+
+export default App;
